a6e0022ce9a9a63bf4bc36e7932d0cf6e2cc57f4
xarielx <a.barboza@ufl.edu>
2019-09-10 22:32:01 -0400

    Adding a file to prove push ability

--- education4less/education4less/settings.py
+++ education4less/education4less/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'scholarships.apps.ScholarshipsConfig',
+    'users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
